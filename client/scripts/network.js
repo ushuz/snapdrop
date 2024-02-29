e350edd2756fb819179eca670825314f0c9b5253
John Hu <hszqqq12@gmail.com>
2024-02-28 21:40:44 -0800

    Update iceServers

--- client/scripts/network.js
+++ client/scripts/network.js
@@ -523,10 +523,10 @@ class Events {
 RTCPeer.config = {
     'sdpSemantics': 'unified-plan',
     'iceServers': [
-      { urls: 'stun:stun.l.google.com:19302' },
       { urls: 'stun:stun1.l.google.com:19302' },
       { urls: 'stun:stun2.l.google.com:19302' },
       { urls: 'stun:stun3.l.google.com:19302' },
       { urls: 'stun:stun4.l.google.com:19302' },
+      { urls: "turn:freestun.net:3479", username: "free", credential: "free" },
     ],
 }
