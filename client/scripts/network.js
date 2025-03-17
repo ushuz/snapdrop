49e5764f1cd2ab8dc8f0c858fb0d96b6f91a5eb4
John Hu <hszqqq12@gmail.com>
2025-03-16 22:27:32 -0700

    Update ICE log format

--- client/scripts/network.js
+++ client/scripts/network.js
@@ -187,12 +187,12 @@ class Peer {
 
     _onChunkReceived(chunk) {
         if(!chunk.byteLength) return;
-        
+
         this._digester.unchunk(chunk);
         const progress = this._digester.progress;
         this._onDownloadProgress(progress);
 
-        // occasionally notify sender about our progress 
+        // occasionally notify sender about our progress
         if (progress - this._lastProgress < 0.01) return;
         this._lastProgress = progress;
         this._sendProgress(progress);
@@ -254,7 +254,7 @@ class RTCPeer extends Peer {
     }
 
     _openChannel() {
-        const channel = this._conn.createDataChannel('data-channel', { 
+        const channel = this._conn.createDataChannel('data-channel', {
             ordered: true,
             reliable: true // Obsolete. See https://developer.mozilla.org/en-US/docs/Web/API/RTCDataChannel/reliable
         });
@@ -325,7 +325,7 @@ class RTCPeer extends Peer {
                 console.error('ICE Gathering failed');
                 break;
             default:
-                console.log('ICE Gathering', this._conn.iceConnectionState);
+                console.log('ICE Gathering:', this._conn.iceConnectionState);
         }
     }
 
